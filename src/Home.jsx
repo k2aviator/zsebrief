923f44fdb62bfd4c47cb0fc19c5ba2bbf7baefb6
Chris Smith <116120269+k2aviator@users.noreply.github.com>
2023-03-26 12:48:02 -0700

    latest updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Nav from './x Nav';
 import { useNavigate } from 'react-router-dom';
 import GetAirportList from './GetAirportList';
 import Header from './Header';
