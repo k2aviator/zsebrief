8054d671aa8f649cef00120c0e5855dc63ec34b1
Chris Smith <116120269+k2aviator@users.noreply.github.com>
2023-05-14 22:16:24 -0700

    hopefully fixed time

--- src/utilTime.js
+++ src/utilTime.js
@@ -4,23 +4,27 @@ export const convertTime12to24 = (time12h) => {
     let timePstOutput = 0;
     if (time12h.length === 10){
         let [time, modifier] = time12h.split(' ');
-        // console.log('length = 10');
-        // console.log("time", time, "modifier", modifier)
+        console.log('length = 10');
+        console.log("time", time, "modifier", modifier)
         let [hours, minutes, secs] = time.split(':')
         
         if (hours === '12' && modifier === 'AM') {
             hours = '00';
             timePstOutput = `${hours}${minutes}`
-        } else if (modifier === 'PM') {
-            hours = parseInt(hours) + 12;
-            //console.log(hours, minutes, secs)
-            timePstOutput = `${hours}${minutes}`
-        } else if (parseInt(hours) < 10 ){
+        } 
+        // EVENTUALLY DELETE
+        // else if (modifier === 'PM') {    
+        //     hours = parseInt(hours) + 12;
+        //     //console.log(hours, minutes, secs)
+        //     timePstOutput = `${hours}${minutes}`
+        // } 
+        else if (parseInt(hours) < 10 ){
             timePstOutput = `0${hours}${minutes}`         
         } else {
+            hours = parseInt(hours) + 12;
             timePstOutput = `${hours}${minutes}`
         }
-        // console.log(timePstOutput)
+        console.log(timePstOutput)
     } else {
             let [time, modifier] = time12h.split(' ');
             let [hours, minutes, secs] = modifier.split(':')
