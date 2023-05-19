9543a1aa93d2a7187f8130b9bd38a7e0626f0eab
Chris Smith <116120269+k2aviator@users.noreply.github.com>
2023-05-19 11:06:57 -0700

    fix

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ export default function Template() {
     return (
 
         <div className="footer-center">
-            <div className="footer-center-left">COPYRIGHT 2023</div>
+            <div className="footer-center-left">COPYRIGHT 2023 | V051723</div>
             <div className="footer-center-right">INQUIRIES @ ZSEBRIEF . COM</div>
         </div>
         
