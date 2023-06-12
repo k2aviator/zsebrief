8effa8926bdb12a032712088130335bd6964e1a2
Chris Smith <116120269+k2aviator@users.noreply.github.com>
2023-06-12 13:23:48 -0700

    turned off line

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,8 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "react/no-unescaped-entities":1,
-        "react/prop-types": 1,
-        "no-unused-vars":1
+        "react/no-unescaped-entities":0,
+        "react/prop-types": 0,
+        "no-unused-vars":0
     }
 }
